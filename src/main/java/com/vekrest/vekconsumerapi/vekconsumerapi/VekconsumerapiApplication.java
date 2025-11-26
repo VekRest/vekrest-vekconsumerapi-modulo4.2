402bfac52f3b9e82c41ffb894e533ce3d85502a3
vek03 <vektromboni@gmail.com>
2025-11-26 15:01:27 -0300

    :tada: First Commit

---
+++ src/main/java/com/vekrest/vekconsumerapi/vekconsumerapi/VekconsumerapiApplication.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekconsumerapi.vekconsumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VekconsumerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VekconsumerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vekrest/vekconsumerapi/vekconsumerapi/VekconsumerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekconsumerapi.vekconsumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VekconsumerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
